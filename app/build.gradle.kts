662ee58f61ecb4b08c5aabb86f7e8b60ef705d22
shahar.lasri <shaharlasri6@gmail.com>
2026-08-21 13:41:29 +0300

    Ordo v2.2.2 - fix launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.shahar.quickcontacts"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.2.0"
+        versionCode = 14
+        versionName = "2.2.2"
     }
 
     buildFeatures {
